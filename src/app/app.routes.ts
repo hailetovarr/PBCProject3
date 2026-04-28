8052a768e2161133b04135a187b97cd077759989
BrennenSindle <139509292+BrennenSindle@users.noreply.github.com>
2026-04-28 13:56:03 -0400

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/summary', pathMatch: 'full' },
+  { 
+    path: 'summary', 
+    loadComponent: () => import('./components/summary/summary.component').then(m => m.SummaryComponent)
+  },
+  { 
+    path: 'income', 
+    loadComponent: () => import('./components/income/income.component').then(m => m.IncomeComponent)
+  },
+  { 
+    path: 'expenses', 
+    loadComponent: () => import('./components/expenses/expenses.component').then(m => m.ExpensesComponent)
+  },
+  { path: '**', redirectTo: '/summary' }
+];
